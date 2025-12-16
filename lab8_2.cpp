f80957f635788c727d92d7da21db2294ffcdb61c
NotThanaphat <thanapat.pingkaew@gmail.com>
2025-12-16 19:35:03 +0700

    upload lab8 22

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -10,7 +10,7 @@ char findGrade(double score)
         return 'A';
     else if (score > 75 && score <= 90)
         return 'B';
-    else if (score > 60 && score < 75)
+    else if (score > 60 && score <= 75)
         return 'C';
     else if (score > 45 && score <= 60)
         return 'D';
